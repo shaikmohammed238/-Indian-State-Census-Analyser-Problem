bd6d19806a3ae73cb0aeb931a8663eb5da1c3fbe
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-24 12:18:09 +0530

    added master

---
+++ day29practce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day29practce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
